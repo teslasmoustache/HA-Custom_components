3ddb1461acaa58559eeaf7f75b446376cd5b8d96
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-05-21 15:57:50 +0200

    cloudflare 1.1.0 - Split functions and general cleanup.

--- cloudflare/custom_components/cloudflare.py
+++ cloudflare/custom_components/cloudflare.py
@@ -11,10 +11,8 @@ import requests
 import voluptuous as vol
 from datetime import timedelta
 from homeassistant.helpers.entity import Entity
-from homeassistant.loader import bind_hass
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import async_track_time_interval
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 DOMAIN = 'cloudflare'
 
@@ -27,7 +25,7 @@ INTERVAL = timedelta(minutes=60)
 SERVICE_UPDATE = 'update_records'
 
 COMPONENT_NAME = 'cloudflare'
-COMPONENT_VERSION = '1.0.1'
+COMPONENT_VERSION = '1.1.0'
 BASE_URL = 'https://api.cloudflare.com/client/v4/zones'
 EXT_IP_URL = 'https://api.ipify.org'
 
@@ -43,36 +41,53 @@ CONFIG_SCHEMA = vol.Schema({
 
 _LOGGER = logging.getLogger(__name__)
 
-@bind_hass
 @asyncio.coroutine
 def async_setup(hass, config):
     email = config[DOMAIN][CONF_EMAIL]
     key = config[DOMAIN][CONF_KEY]
     zone = config[DOMAIN][CONF_ZONE]
     records = config[DOMAIN][CONF_RECORDS]
-    session = async_get_clientsession(hass)
 
     @asyncio.coroutine
     def update_domain_interval(now):
-        """Update the Cloudflare entry."""
-        _update_cloudflare(session, email, key, zone, records)
+        result = yield from _update_cloudflare(email, key, zone, records)
+        if not result:
+            return False
 
     @asyncio.coroutine
     def update_domain_service(call):
-        """Update the Cloudflare entry."""
-        _update_cloudflare(session, email, key, zone, records)
+        result = yield from _update_cloudflare(email, key, zone, records)
+        if not result:
+            return False
 
     async_track_time_interval(hass, update_domain_interval, INTERVAL)
     hass.services.async_register(
         DOMAIN, SERVICE_UPDATE, update_domain_service)
     return True
 
-def _update_cloudflare(session, email, key, zone, records):
-    _LOGGER.info('Starting update for zone %s', zone)
-    IP = requests.get(EXT_IP_URL).text
-    headers = {'X-Auth-Email': email, 'X-Auth-Key': key , 'Content-Type': 'application/json'}
+@asyncio.coroutine
+def _update_cloudflare(email, key, zone, records):
+    _LOGGER.info('Starting update for zone %s.', zone)
+    headers = {
+        'X-Auth-Email': email,
+        'X-Auth-Key': key, 
+        'Content-Type': 'application/json'
+        }
+    yield from _update_cloudflare_get_zone(headers, zone, records)
+    _LOGGER.info('Update for zone %s is complete.', zone)
+    return True
+    
+@asyncio.coroutine
+def _update_cloudflare_get_zone(headers, zone, records):
+    _LOGGER.debug('Starting function _update_cloudflare_get_zone')
     zoneIDurl = BASE_URL + '?name=' + zone
     zoneID = requests.get(zoneIDurl, headers=headers).json()['result'][0]['id']
+    yield from _update_cloudflare_get_record(headers, zoneID, records, zone)
+    return True
+    
+@asyncio.coroutine
+def _update_cloudflare_get_record(headers, zoneID, records, zone):
+    _LOGGER.debug('Starting function _update_cloudflare_get_record')
     if 'None' in records:
         _LOGGER.debug('Records not defined, scanning for records...')
         getRecordsUrl = BASE_URL + '/' + zoneID + '/dns_records&per_page=100'
@@ -89,9 +104,8 @@ def _update_cloudflare(session, email, key, zone, records):
             RecordFullname = record
         else:
             RecordFullname = record + '.' + zone
-        _LOGGER.info('Updating record %s', RecordFullname)
         recordIDurl = BASE_URL + '/' + zoneID + '/dns_records?name=' + RecordFullname
-        recordInfo = requests.get(recordIDurl, headers=headers, timeout=10).json()['result'][0]
+        recordInfo = requests.get(recordIDurl, headers=headers).json()['result'][0]
         RecordID = recordInfo['id']
         RecordType = recordInfo['type']
         RecordProxy = str(recordInfo['proxied'])
@@ -100,15 +114,26 @@ def _update_cloudflare(session, email, key, zone, records):
             proxied = True
         else:
             proxied = False
-        data = json.dumps({'id': zoneID, 'type': RecordType, 'name': RecordFullname, 'content': IP, 'proxied': proxied})
-        fetchurl = BASE_URL + '/' + zoneID + '/dns_records/' + RecordID 
-        if RecordContent == IP:
-            _LOGGER.debug('The IP for %s has not changed, skipping update', RecordFullname)
+        yield from _update_cloudflare_update_record(headers, zoneID, RecordID, RecordType, RecordContent, proxied, RecordFullname)
+    return True
+
+@asyncio.coroutine
+def _update_cloudflare_update_record(headers, zoneID, RecordID, RecordType, RecordContent, proxied, RecordFullname):
+    _LOGGER.debug('Starting function _update_cloudflare_update_record for record %s', RecordFullname)
+    IP = requests.get(EXT_IP_URL).text
+    data = json.dumps({
+        'id': zoneID,
+        'type': RecordType,
+        'name': RecordFullname,
+        'content': IP,
+        'proxied': proxied
+        })
+
+    fetchurl = BASE_URL + '/' + zoneID + '/dns_records/' + RecordID 
+    if RecordContent != IP:
+        if RecordType == 'A':
+            result = requests.put(fetchurl, headers=headers, data=data).json()
+            _LOGGER.debug('Update successfully: %s', result['success'])
         else:
-            if RecordType == 'A':
-                result = requests.put(fetchurl, headers=headers, data=data).json()
-                _LOGGER.debug('Update successfully: %s', result['success'])
-            else:
-                _LOGGER.debug('Record type for %s is not A, skipping update', RecordFullname)
-    _LOGGER.info('Update for zone %s is complete.', zone)
-    return str(True)
+            _LOGGER.debug('Record type for %s is not A, skipping update', RecordFullname)
+    return True
