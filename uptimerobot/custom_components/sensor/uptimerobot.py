dfdd1ecc1326c2bf2d1996efbbe65d9a1874f98b
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-05-25 21:54:54 +0200

    uptimerobot - tox compliant

--- uptimerobot/custom_components/sensor/uptimerobot.py
+++ uptimerobot/custom_components/sensor/uptimerobot.py
@@ -1,18 +1,18 @@
 """
 A component to monitor Uptime Robot monitors.
+
 For more details about this component, please refer to the documentation at
 https://github.com/HalfDecent/HA-Custom_components/uptimerobot
 """
 import logging
-import voluptuous as vol
 from datetime import timedelta
-from homeassistant.helpers.entity import Entity
+import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import CONF_API_KEY
+from homeassistant.helpers.entity import Entity
 
 REQUIREMENTS = ['pyuptimerobot==0.0.4']
-
 
 ATTR_TARGET = 'target'
 ATTR_MONITORID = 'monitor id'
@@ -22,8 +22,6 @@ ATTR_COMPONENT_VERSION = 'component_version'
 SCAN_INTERVAL = timedelta(seconds=30)
 
 ICON = 'mdi:server'
-COMPONENT_NAME = 'uptimerobot'
-COMPONENT_VERSION = '1.0.3'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
@@ -31,44 +29,48 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 _LOGGER = logging.getLogger(__name__)
 
+
 def setup_platform(hass, config, add_devices_callback, discovery_info=None):
     """Set up the Uptime Robot sensors."""
     from pyuptimerobot import UptimeRobot
-    ur = UptimeRobot()
+    uptimerobot = UptimeRobot()
     apikey = config.get(CONF_API_KEY)
-    monitors = ur.getMonitors(apikey)
+    monitors = uptimerobot.getMonitors(apikey)
     _LOGGER.debug(monitors)
     dev = []
-    if monitors == False:
-        _LOGGER.critical('Something terrible happend :(')
-        return False
+    if not monitors:
+        _LOGGER.critical("Something terrible happend :(")
+        retval = False
     else:
         for monitor in monitors['monitors']:
             _LOGGER.debug(monitor)
             monitorid = monitor['id']
             name = monitor['friendly_name']
             target = monitor['url']
-            dev.append(UptimeRobotSensor(apikey, monitorid, name, target, ur))
+            dev.append(UptimeRobotSensor(apikey, monitorid,
+                                         name, target, uptimerobot))
         add_devices_callback(dev, True)
-        return True
+        retval = True
+    return retval
+
 
 class UptimeRobotSensor(Entity):
     """Representation of a Uptime Robot sensor."""
-    def __init__(self, apikey, monitorid, name, target, ur):
+
+    def __init__(self, apikey, monitorid, name, target, uptimerobot):
+        """Initialize the sensor."""
         self._name = name
         self._monitorid = monitorid
         self._apikey = apikey
-        self._ur = ur
+        self._ur = uptimerobot
         self._target = target
-        self._component = COMPONENT_NAME
-        self._componentversion = COMPONENT_VERSION  
         self.update()
 
     def update(self):
         """Get the latest state of the sensor."""
         monitor = self._ur.getMonitors(self._apikey, str(self._monitorid))
-        if monitor == False:
-            _LOGGER.debug('Failed to get new state, trying again later.')
+        if not monitor:
+            _LOGGER.debug("Failed to get new state, trying again later.")
             return False
         else:
             monitorinfo = monitor['monitors'][0]
@@ -90,7 +92,7 @@ class UptimeRobotSensor(Entity):
 
     @property
     def icon(self):
-        """Return the icon to use in the frontend, if any."""
+        """Return the icon to use in the frontend."""
         return ICON
 
     @property
@@ -99,6 +101,4 @@ class UptimeRobotSensor(Entity):
         return {
             ATTR_TARGET: self._target,
             ATTR_MONITORID: self._monitorid,
-            ATTR_COMPONENT: self._component,
-            ATTR_COMPONENT_VERSION: self._componentversion
         }
